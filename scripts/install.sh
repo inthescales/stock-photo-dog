669910806ee930c435f856dccb6b3bb22d396b29
Robin Hill <rehill012@gmail.com>
2022-11-06 19:30:22 -0500

    Update installer

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
-pip3 install -r ../requirements.txt
+pip3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
 crontab -l > tmp
-echo "*/2 * * * * sh /var/www/bots/stock-photo-dog/scripts/run.sh" >> tmp
+echo "*/2 * * * * bash /var/www/bots/stock-photo-dog/scripts/run.sh" >> tmp
 crontab tmp
 rm tmp
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,3 @@
 cd /var/www/bots/stock-photo-dog/
+source env/bin/activate
 python3 /var/www/bots/stock-photo-dog/dog.py --publish
